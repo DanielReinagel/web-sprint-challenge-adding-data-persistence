5dd39f5c1e407acbf89e772b68c4487a5c8ea118
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-11-05 13:13:40 -0700

    proper project_id verification in posting to tasks route

--- api/task/router.js
+++ api/task/router.js
@@ -2,17 +2,24 @@ const express = require("express");
 
 const model = require("./model");
 
-const checkBody = (req, res, next) => { //TODO verify project_id
+const checkBody = async (req, res, next) => {
   const { task_description, task_notes, task_completed, project_id } = req.body;
   if(!task_description) res.status(400).json({message:"You must provide a task description"});
   else if(!project_id) res.status(400).json({message:"You must provide a project id"});
   else {
-    const p = {}; req.payload = p;
-    p.task_description = task_description;
-    p.project_id = project_id
-    if(task_notes) p.task_notes = task_notes;
-    if(task_completed) p.task_completed = task_completed;
-    next();
+    let idExists = false; let error = false;
+    await require("../project/model").getAll()
+      .then(projects => idExists = projects.reduce((acc, p) => acc||p.project_id===project_id, false))
+      .catch(err => {error=true; res.status(500).json({shortHand:"error getting projects from database", message:err.message, stack:err.stack, error:err});}); //On one line because this line of code should never be run and should generally be ignored.
+
+    if(idExists){
+      const p = {}; req.payload = p;
+      p.task_description = task_description;
+      p.project_id = project_id
+      if(task_notes) p.task_notes = task_notes;
+      if(task_completed) p.task_completed = task_completed;
+      next();
+    } else if(!error) res.status(400).json({message:"You must input a valid project_id"});
   }
 }
 
