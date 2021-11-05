561db6e01d5529c442b7382861f15f91a2194d19
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-11-05 11:25:19 -0700

    get requests actually responding properly

--- api/project/model.js
+++ api/project/model.js
@@ -1,7 +1,7 @@
 //Projects Model
 const db = require("../../data/dbConfig");
 
-const getAll = () => db("projects");
+const getAll = () => db("projects").then(projects => projects.map(project => {return{...project, project_completed:!!project.project_completed}}));
 
 const insert = project => db("projects").insert(project).then(([id]) => {return {id, ...project}});
 
--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,10 @@
 //Tasks Model
 const db = require("../../data/dbConfig");
 
-const getAll = () => db("tasks");
+const getAll = () => db("tasks")
+  .leftJoin("projects", "tasks.project_id", "projects.project_id")
+  .select("tasks.task_id", "tasks.task_description", "tasks.task_notes", "tasks.task_completed", "projects.project_name", "projects.project_description")
+  .then(tasks => tasks.map(task => {return{...task, task_completed:!!task.task_completed}}));
 
 const insert = task => db("tasks").insert(task).then(([id]) => {return {id, ...task}});
 
